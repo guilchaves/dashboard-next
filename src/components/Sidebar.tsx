4fcf7428bd1902d0fc5b948f868e7ee300ed8a17
guilherme chaves <chaves.guilherme@proton.me>
2025-02-17 17:24:34 -0300

    chore: adiciona user item em sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import UserItem from "./UserItem";
+
+export default function Sidebar() {
+  return <div className="flex flex-col gap-4 w-[300px] min-w-[300] border-r min-h-screen p-4">
+    <div>
+      <UserItem />
+    </div>
+    <div className="grow">Menu</div>
+    <div>Settings / Notifications </div>
+  </div>
+}
---
+++ src/components/UserItem.tsx
@@ -0,0 +1,17 @@
+"use client";
+export default function UserItem() {
+  return (
+    <div className="flex items-center justify-between gap-2 border rounded-[8px] p-2">
+      <div
+        className="avatar rounded-full min-h-9 min-w-9 bg-emerald-500 text-white font-[700] flex items-center 
+        justify-center"
+      >
+        <p>GD</p>
+      </div>
+      <div className="grow">
+        <p className="text-base font-bold">Guilherme Chaves</p>
+        <p className="text-sm text-neutral-500">gchaves.guilherme@gmail.com</p>
+      </div>
+    </div>
+  );
+}
