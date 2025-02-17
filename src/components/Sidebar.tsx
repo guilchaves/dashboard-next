9edec9c89310e7194cd37f577b21cb0e36cd59ec
guilherme chaves <chaves.guilherme@proton.me>
2025-02-17 17:49:30 -0300

    chore: adiciona command em sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,68 @@
-'use client';
+"use client";
 
 import UserItem from "./UserItem";
+import {
+  Command,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  User,
+  Inbox,
+  CreditCard,
+  Bell,
+  Settings,
+  Cookie,
+  MessageSquare,
+} from "lucide-react";
 
 export default function Sidebar() {
-  return <div className="flex flex-col gap-4 w-[300px] min-w-[300] border-r min-h-screen p-4">
-    <div>
-      <UserItem />
+  const menuList = [
+    {
+      group: "General",
+      items: [
+        { link: "/", icon: <User />, text: "Profile" },
+        { link: "/", icon: <Inbox />, text: "Inbox" },
+        { link: "/", icon: <CreditCard />, text: "Billing" },
+        { link: "/", icon: <Bell />, text: "Notifications" },
+      ],
+    },
+    {
+      group: "Settings",
+      items: [
+        { link: "/", icon: <Settings />, text: "General Settings" },
+        { link: "/", icon: <Cookie />, text: "Privacy" },
+        { link: "/", icon: <MessageSquare />, text: "Logs" },
+      ],
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-4 w-[300px] min-w-[300] border-r min-h-screen p-4">
+      <div>
+        <UserItem />
+      </div>
+      <div className="grow">
+        <Command style={{ overflow: "visible" }}>
+          <CommandList style={{ overflow: "visible" }}>
+            {menuList.map((menu: any, key: number) => (
+              <CommandGroup key={key} heading={menu.group}>
+                {menu.items.map((option: any, optionKey: number) => (
+                  <CommandItem
+                    key={optionKey}
+                    className="flex gap-2 cursor-pointer"
+                  >
+                    {option.icon}
+                    {option.text}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            ))}
+          </CommandList>
+        </Command>
+      </div>
+      <div>Settings / Notifications </div>
     </div>
-    <div className="grow">Menu</div>
-    <div>Settings / Notifications </div>
-  </div>
+  );
 }
