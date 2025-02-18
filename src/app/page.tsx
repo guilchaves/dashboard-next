fcc4c3403ce7cf6889667704d980b8bb21a72506
guilherme chaves <chaves.guilherme@proton.me>
2025-02-18 15:27:11 -0300

    chore: adiciona data table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Calendar from "@/components/Cards/Calendar";
+import { DataTableDemo } from "@/components/Cards/DataTable";
 import General from "@/components/Cards/General";
 import Lines from "@/components/Cards/Lines";
 import TableComponent from "@/components/Cards/Table";
@@ -32,7 +33,15 @@ export default function Home() {
             <TableComponent />
           </CardContent>
         </Card>
-        <Card>Hello world</Card>
+        <Card className="overflow-y-scroll">
+          <CardHeader>
+            <CardTitle>Users</CardTitle>
+            <CardDescription>These are the users of this month.</CardDescription>
+          </CardHeader>
+          <CardContent className="">
+            <DataTableDemo />
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
